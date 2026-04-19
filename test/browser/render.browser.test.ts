a990ab47f681fc1ede8330fc0e9a5d150f839dd1
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-04-19 20:38:48 -0300

    examples running

--- test/browser/render.browser.test.ts
+++ test/browser/render.browser.test.ts
@@ -1,6 +1,7 @@
 import { describe, test } from "vitest";
 import { createCanvas2DContext, Rect, Scene, FragmentShader } from "../../src";
 import pulseFragmentShader from "./pulse.fragment.wgsl?raw";
+import singularityFragmentShader from "./singularity.fragment.wgsl?raw";
 import { detectWebGPUSupport } from "./support";
 
 describe("WebGPUCanvas2DContext", () => {
@@ -54,5 +55,55 @@ describe("WebGPUCanvas2DContext", () => {
     });
 
     // context.destroy();
+  });
+
+  test("singularity shader example", async () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 400;
+    canvas.height = 400;
+    canvas.style.width = "400px";
+    canvas.style.height = "400px";
+    canvas.style.background = "black";
+    document.body.appendChild(canvas);
+
+    const support = await detectWebGPUSupport();
+
+    if (support !== "usable") {
+      return;
+    }
+
+    const context = await createCanvas2DContext(canvas);
+    const fragmentShader = FragmentShader.new({
+      source: singularityFragmentShader,
+    });
+
+    fragmentShader.setUniforms({
+      speed: 1,
+      intensity: 1,
+      size: 1,
+      waveStrength: 1,
+      colorShift: 1,
+      u_resolution_x: 400,
+      u_resolution_y: 400,
+    });
+
+    context.clear({ r: 0, g: 0, b: 0, a: 1 });
+
+    const rect = new Rect({
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 400,
+      fragmentShader,
+      fragmentShaderEntryPoint: "main",
+    });
+
+    const scene = new Scene();
+    scene.add(rect);
+
+    await context.draw(scene);
+    context.loop(30_000, async () => {
+      await context.draw(scene);
+    });
   });
 });
