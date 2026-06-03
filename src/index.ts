27fe090797dc70f3cfedfd946e1d39f6b96ab91e
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-06-02 21:54:12 -0300

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// import { example } from "./examples/compute-shader/compute-shader";
-// import { example } from "./examples/average-histogram/average-histogram";
 import { rasterizingExample } from "./examples/rasterizing/rasterizing";
-// import { cpuExample } from "./examples/oldExamples";
 
 export { rasterizingExample as example };
